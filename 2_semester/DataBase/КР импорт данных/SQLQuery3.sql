702629bdf7f7e6ca81d719e913971f6c089afa20
Аганичев Александр Георгиевич <AganichevAG@dom201.local>
2025-04-22 14:06:09 +0700

    22.04 DB

---
+++ 2_semester/DataBase/КР импорт данных/SQLQuery3.sql
@@ -0,0 +1,9 @@
+Alter table Books
+add foreign key (fk_GenreID) references Genres(Id);
+
+alter table Loans 
+add foreign key (fk_UserID) references Users(UsersId),
+foreign key(fk_BookID) references Books(ID);
+
+alter table Authors_Books
+add foreign key(fk_AuthorID) references Authors(AuthorID);
