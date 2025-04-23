2feb35c0693a380e434758ba8f39daa2cef6d432
Саша Аганичев <aganichevAG@mer.ci.nsu.ru>
2025-04-23 07:17:27 +0700

    Добавил связь

--- 2_semester/DataBase/КР импорт данных/SQLQuery3.sql
+++ 2_semester/DataBase/КР импорт данных/SQLQuery3.sql
@@ -6,4 +6,5 @@ add foreign key (fk_UserID) references Users(UsersId),
 foreign key(fk_BookID) references Books(ID);
 
 alter table Authors_Books
-add foreign key(fk_AuthorID) references Authors(AuthorID);
+add foreign key(fk_AuthorID) references Authors(AuthorID),
+foreign key(fk_BookID) references Books(ID);
